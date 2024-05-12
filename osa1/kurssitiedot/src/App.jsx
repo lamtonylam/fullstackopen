136cabf5ba5806cb785a2b0f664385f4dfa999c0
Tony Lam <tony.lam@iki.fi>
2024-05-12 22:03:12 +0300

    1.3 - 1.5

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,23 +1,27 @@
 const App = () => {
-    const course = "Half Stack application development";
-    const part1 = "Fundamentals of React";
-    const exercises1 = 10;
-    const part2 = "Using props to pass data";
-    const exercises2 = 7;
-    const part3 = "State of a component";
-    const exercises3 = 14;
-
-    const courses = [
-        { part: "Fundamentals of React", exercises: 10 },
-        { part: "Using props to pass data", exercises: 7 },
-        { part: "State of a component", exercises: 14 },
-    ];
+    const course = {
+        name: "Half Stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7,
+            },
+            {
+                name: "State of a component",
+                exercises: 14,
+            },
+        ],
+    };
 
     return (
         <div>
             <Header course={course} />
-            <Content courses={courses} />
-            <Total courses={courses} />
+            <Content courses={course} />
+            <Total courses={course} />
         </div>
     );
 };
@@ -25,7 +29,7 @@ const App = () => {
 const Header = (props) => {
     return (
         <>
-            <h1>{props.course}</h1>
+            <h1>{props.course.name}</h1>
         </>
     );
 };
@@ -34,13 +38,22 @@ const Content = (props) => {
     return (
         <>
             <p>
-                <Part course={props.courses[0]} />
+                <Part
+                    course_name={props.courses.parts[0].name}
+                    exercises={props.courses.parts[0].exercises}
+                />
             </p>
             <p>
-                <Part course={props.courses[1]} />
+                <Part
+                    course_name={props.courses.parts[1].name}
+                    exercises={props.courses.parts[1].exercises}
+                />
             </p>
             <p>
-                <Part course={props.courses[2]} />
+                <Part
+                    course_name={props.courses.parts[2].name}
+                    exercises={props.courses.parts[2].exercises}
+                />
             </p>
         </>
     );
@@ -49,15 +62,15 @@ const Content = (props) => {
 const Part = (props) => {
     return (
         <>
-            {props.course.part} {props.course.exercises}
+            {props.course_name} {props.exercises}
         </>
     );
 };
 
 const Total = (props) => {
-    const amount0 = props.courses[0].exercises;
-    const amount1 = props.courses[1].exercises;
-    const amount2 = props.courses[2].exercises;
+    const amount0 = props.courses.parts[0].exercises;
+    const amount1 = props.courses.parts[1].exercises;
+    const amount2 = props.courses.parts[2].exercises;
 
     return <>Number of exercises {amount0 + amount1 + amount2}</>;
 };
