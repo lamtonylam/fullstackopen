56706f22bbc170120cdd9952d2c6e628fe486add
Tony Lam <tony.lam@iki.fi>
2024-07-10 18:40:49 +0200

    undo dont cannot delete index.html

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -68,9 +68,13 @@ const App = () => {
                 number: newNumber,
             };
 
-            setPersons(persons.concat(personObject));
-            setNewName("");
-            setNewNumber("");
+            axios
+                .post("http://localhost:3001/persons", personObject)
+                .then((response) => {
+                    setPersons(persons.concat(response.data));
+                    setNewName("");
+                    setNewName("");
+                });
         }
     };
 
