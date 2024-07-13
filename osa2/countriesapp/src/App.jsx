734e7ed30514f3d0426f3aa61bf9b738d518167a
Tony Lam <tony.lam@iki.fi>
2024-07-13 18:10:42 +0200

    2.20*: maiden tiedot, step3

--- osa2/countriesapp/src/App.jsx
+++ osa2/countriesapp/src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+const api_key = import.meta.env.VITE_SOME_KEY;
 
 const App = () => {
     const [countries, setCountries] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const [weather, setWeather] = useState([]);
 
     // this gets all countries
     useEffect(() => {
@@ -16,16 +18,39 @@ const App = () => {
         }
     }, [searchTerm]);
 
+    // this filters the countries based on the form
+    let countriesToShow = countries.filter((country) =>
+        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    // fetch the weather data
+    const fetchWeatherData = () => {
+        if (searchTerm && countriesToShow.length > 0) {
+            axios
+                .get(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${countriesToShow[0]?.capital}&appid=${api_key}`
+                )
+                .then((response) => {
+                    setWeather(response.data);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    };
+
+    // only get call weatherdata once searchterm is changed
+    useEffect(fetchWeatherData, [searchTerm]);
+
     // this handles the search form change
     const handleForm = (event) => {
         event.preventDefault();
         setSearchTerm(event.target.value);
     };
 
-    // this filters the countries based on the form
-    let countriesToShow = countries.filter((country) =>
-        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    // weather handling
+    const temperature = ((weather?.main?.temp - 273.15) * 100) / 100;
+    const roundedTemperature = temperature.toFixed(2);
 
     // this is for the show button
     const showCountry = (country) => {
@@ -53,6 +78,14 @@ const App = () => {
                     )}
                     <br></br>
                     <img src={countriesToShow[0].flags.png} height="100px" />
+                    <h2>Weather in {countriesToShow[0].capital}</h2>
+                    <p>temperature {roundedTemperature} celcius</p>
+                    {countriesToShow.length === 1 && weather?.weather ? (
+                        <img
+                            src={`https://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`}
+                        />
+                    ) : null}
+                    <p>wind {weather?.wind?.speed} m/s</p>
                 </div>
             ) : null}
 
