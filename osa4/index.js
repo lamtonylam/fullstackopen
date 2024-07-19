5b658414429c12b9fca0d99b9ef3e4c561208146
Tony Lam <tony.lam@iki.fi>
2024-07-19 21:44:02 +0200

    4.2 blogilista, step2 routes handling into own module

--- osa4/index.js
+++ osa4/index.js
@@ -3,28 +3,21 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+// modules
 const config = require("./utils/config");
 const Blog = require("./models/blog");
+const blogsRouter = require("./controllers/blogs")
 
+// mongoose
 const mongoUrl = `mongodb+srv://tonylam:${config.password}@cluster.6hp2tkl.mongodb.net/osa4?retryWrites=true&w=majority`;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs);
-    });
-});
+// routes modules that handles all the routes
+app.use("/api/blogs/", blogsRouter)
 
-app.post("/api/blogs", (request, response) => {
-    const blog = new Blog(request.body);
-
-    blog.save().then((result) => {
-        response.status(201).json(result);
-    });
-});
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
