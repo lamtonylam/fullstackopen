c5fb263477e1201642b98db41777a30fd785f446
Tony Lam <tony.lam@iki.fi>
2024-07-28 17:41:23 +0300

    chore: Refactor login and blog to own components

--- osa5/src/App.jsx
+++ osa5/src/App.jsx
@@ -5,6 +5,51 @@ import Blog from './components/Blog';
 import blogService from './services/blogs';
 import loginService from './services/login';
 
+const Login = ({
+  handleLogin,
+  username,
+  password,
+  setUsername,
+  setPassword,
+}) => {
+  return (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type='text'
+            value={username}
+            name='Username'
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type='password'
+            value={password}
+            name='Password'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  );
+};
+
+const BlogList = ({ user, blogs }) => (
+  <div>
+    <h2>blogs</h2>
+    <p>{user.name} logged in</p>
+    {blogs.map(blog => (
+      <Blog key={blog.id} blog={blog} />
+    ))}
+  </div>
+);
+
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState('');
@@ -41,42 +86,17 @@ const App = () => {
 
   if (user === null) {
     return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-            <input
-              type='text'
-              value={username}
-              name='Username'
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-            <input
-              type='password'
-              value={password}
-              name='Password'
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type='submit'>login</button>
-        </form>
-      </div>
+      <Login
+        handleLogin={handleLogin}
+        username={username}
+        password={password}
+        setPassword={setPassword}
+        setUsername={setUsername}
+      />
     );
   }
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      <p> {user.name} logged in</p>
-      {blogs.map(blog => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
-    </div>
-  );
+  return <BlogList user={user} blogs={blogs} />;
 };
 
 export default App;
