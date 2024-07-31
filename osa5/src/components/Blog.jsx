9f59e7d7fb375bcea3583773d9520eddd2f65470
Tony Lam <tony.lam@iki.fi>
2024-07-31 18:55:14 +0300

    5.7 blogilistan frontend, step7 Lisää yksittäiselle blogille nappi, jonka avulla voi kontrolloida, näytetäänkö kaikki blogiin liittyvät tiedot.
    Klikkaamalla nappia sen täydelliset tiedot aukeavat

--- osa5/src/components/Blog.jsx
+++ osa5/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? 'none' : '' };
+  const showWhenVisible = { display: visible ? '' : 'none' };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        <button onClick={toggleVisibility}>hide</button>
+        <p>{blog.url}</p>
+        <p>
+          {blog.likes}
+          <button>like</button>
+        </p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
