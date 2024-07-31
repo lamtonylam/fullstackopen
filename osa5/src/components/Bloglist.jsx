b60401cac6a1ec138dd97c060a7eb82e6c8b060c
Tony Lam <tony.lam@iki.fi>
2024-07-31 20:44:36 +0300

    5.10: blogilistan frontend, step10 Järjestä sovellus näyttämään blogit likejen mukaisessa suuruusjärjestyksessä.

--- osa5/src/components/Bloglist.jsx
+++ osa5/src/components/Bloglist.jsx
@@ -2,13 +2,21 @@ import Blog from './Blog';
 import CreateBlog from './Create_blog';
 import Togglable from './Toggable';
 
-const BlogList = ({ user, blogs }) => (
-  <div>
+const BlogList = ({ user, blogs }) => {
+  // sort by likes
+  blogs.sort(function (a, b) {
+    return a.likes - b.likes;
+  });
 
-    {blogs.map(blog => (
-      <Blog key={blog.id} blog={blog} />
-    ))}
-  </div>
-);
+  blogs.reverse();
+
+  return (
+    <div>
+      {blogs.map(blog => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
 
 export default BlogList;
