30846042f8c14b3a290bb1bcf522e4df82ac46ee
Tony Lam <tony.lam@iki.fi>
2024-08-03 17:15:34 +0300

    5.14: blogilistan testit, step2 Tee testi, joka varmistaa että myös url, likejen määrä ja käyttäjä näytetään, kun blogin kaikki tiedot näyttävää nappia on painettu.

--- osa5/src/components/Blog.test.jsx
+++ osa5/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 import { expect, test } from 'vitest';
 
@@ -17,4 +18,31 @@ test('renders blog title and author', () => {
 
   const element = screen.getByText('testi blogi Mikko Mallikas');
   expect(element).toBeDefined();
+});
+
+test('expanding with click shows details', async () => {
+  const blog = {
+    user: {
+      name: 'Tony',
+    },
+    title: 'testi blogi',
+    author: 'Mikko Mallikas',
+    url: 'hs.fi',
+    likes: 40,
+  };
+
+  render(<Blog blog={blog} />);
+
+  // click button
+  const user = userEvent.setup();
+  const button = screen.getByText('view');
+  await user.click(button);
+
+  // get items to be tested
+  const url = screen.getByText('hs.fi');
+  const likes = screen.getByText('40');
+  const user_test = screen.getByText('Tony');
+
+  // assert that they are there
+  expect(url, likes, user_test).toBeDefined();
 });
