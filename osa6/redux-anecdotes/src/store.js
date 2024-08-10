2ea5dd10baef18b667e4e2f2179e2897f76d0e7f
Tony Lam <tony.lam@iki.fi>
2024-08-10 22:10:53 +0300

    6.12 anekdootit, step10 notification component and reducer.

--- osa6/redux-anecdotes/src/store.js
+++ osa6/redux-anecdotes/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducers/anecdoteReducer';
 import filterReducer from './reducers/filterReducer';
+import notificationReducer from './reducers/notificationReducer';
 
 const combinedStore = configureStore({
   reducer: {
     anecdotes: reducer,
-    filter: filterReducer
+    filter: filterReducer,
+    notification: notificationReducer
   }
 });
 
