2025324218694fc21732e75bdecfec1bf2f08680
Tony Lam <tony.lam@iki.fi>
2024-08-12 12:58:25 +0300

    6.16 anekdootit ja backend, step3 Muuta Redux-storen alustus tapahtumaan Redux Thunk ‑kirjaston avulla toteutettuun asynkroniseen actioniin.

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -3,15 +3,14 @@ import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Filter from './components/Filter';
 import Notification from './components/Notification';
-import anecdoteService from './services/anecdotes';
-import { setAnecdotes } from './reducers/anecdoteReducer';
 import { useDispatch } from 'react-redux';
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
-  // get all notes from json backend
+  // get all anecdotes from json backend
   const dispatch = useDispatch();
   useEffect(() => {
-    anecdoteService.getAll().then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+    dispatch(initializeAnecdotes());
   }, []);
 
   return (
