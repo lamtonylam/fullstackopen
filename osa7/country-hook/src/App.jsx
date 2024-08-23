3bd8c59556058b782438b21822a49fa68d5ff321
Tony Lam <tony.lam@iki.fi>
2024-08-23 14:30:09 +0300

    7.9: koodin automaattinen muotoilu

--- osa7/country-hook/src/App.jsx
+++ osa7/country-hook/src/App.jsx
@@ -1,94 +1,92 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const useField = (type) => {
-    const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
 
-    const onChange = (event) => {
-        setValue(event.target.value);
-    };
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
 
-    return {
-        type,
-        value,
-        onChange,
-    };
+  return {
+    type,
+    value,
+    onChange,
+  };
 };
 
 const useCountry = (name) => {
-    const [country, setCountry] = useState(null);
+  const [country, setCountry] = useState(null);
 
-    useEffect(() => {
-        if (name) {
-            axios
-                .get(
-                    `https://studies.cs.helsinki.fi/restcountries/api/name/${name}`
-                )
-                .then((response) => {
-                    setCountry({
-                        found: true,
-                        data: {
-                            name: response.data.name.common,
-                            capital: response.data.capital[0],
-                            population: response.data.population,
-                            flag: response.data.flags.png,
-                        },
-                    });
-                })
-                .catch((error) => {
-                    setCountry({
-                        found: false,
-                    });
-                });
-        }
-    }, [name]);
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+        .then((response) => {
+          setCountry({
+            found: true,
+            data: {
+              name: response.data.name.common,
+              capital: response.data.capital[0],
+              population: response.data.population,
+              flag: response.data.flags.png,
+            },
+          });
+        })
+        .catch((error) => {
+          setCountry({
+            found: false,
+          });
+        });
+    }
+  }, [name]);
 
-    return country;
+  return country;
 };
 
 const Country = ({ country }) => {
-    if (!country) {
-        return null;
-    }
+  if (!country) {
+    return null;
+  }
 
-    if (!country.found) {
-        return <div>not found...</div>;
-    }
+  if (!country.found) {
+    return <div>not found...</div>;
+  }
 
-    return (
-        <div>
-            <h3>{country.data.name} </h3>
-            <div>capital {country.data.capital} </div>
-            <div>population {country.data.population}</div>
-            <img
-                src={country.data.flag}
-                height="100"
-                alt={`flag of ${country.data.name}`}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <h3>{country.data.name} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flag}
+        height="100"
+        alt={`flag of ${country.data.name}`}
+      />
+    </div>
+  );
 };
 
 const App = () => {
-    const nameInput = useField("text");
-    const [name, setName] = useState("");
-    const country = useCountry(name);
+  const nameInput = useField('text');
+  const [name, setName] = useState('');
+  const country = useCountry(name);
 
-    const fetch = (e) => {
-        e.preventDefault();
-        setName(nameInput.value);
-    };
+  const fetch = (e) => {
+    e.preventDefault();
+    setName(nameInput.value);
+  };
 
-    return (
-        <div>
-            <form onSubmit={fetch}>
-                <input {...nameInput} />
-                <button>find</button>
-            </form>
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
 
-            <Country country={country} />
-        </div>
-    );
+      <Country country={country} />
+    </div>
+  );
 };
 
 export default App;
